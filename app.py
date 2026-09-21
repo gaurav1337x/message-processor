c28c9467352a70506d0e08fee6dfb29a26d61f61
gaurav1337x <bhartig421@gmail.com>
2026-09-21 18:12:25 +0530

    Streamlit UI

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-print("Message Processor Application") 
-print("Version 2.0")
-print("Updates work correctly")
+# print("Message Processor Application") 
+# print("Version 2.0")
+# print("Updates work correctly")
+
+import streamlit as st 
+st.title("Message Processor")
+message = st.text_input("Enter your message")
+if st.button("Process Message"):
+    st.write("You entered:")
+    st.write(message) 
