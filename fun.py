a8df27c82dc8edd63cd9b2bbb0be76e9f4e4f4fa
gaurav1337x <bhartig421@gmail.com>
2026-09-21 18:20:56 +0530

    Seconf Fun app

--- fun.py
+++ fun.py
@@ -1,3 +1,10 @@
-print("Message Processor Application") 
-print("Version 2.0")
-print("Updates work correctly")
+# print("Message Processor Application") 
+# print("Version 2.0")
+# print("Updates work correctly")
+
+import streamlit as st 
+st.title("Fun Application")
+message = st.text_input("Enter Anything")
+if st.button("Go"):
+    st.write("You entered:")
+    st.write("Gali") 
